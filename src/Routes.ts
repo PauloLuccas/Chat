12b2d73730494ca04d8a291421c46e320d71977a
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2021-04-27 12:29:45 -0300

    configurando rotas

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,34 +1,10 @@
-import { request, response, Router } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingRepository } from './repositories/SettingsRepositoy';
+import { Router } from 'express';
+import { SettingController } from './controllers/SettingsController';
 
 const routes = Router();
 
-/**
- * TIPOS DE PARÂMETROS
- * Routes Params -> Parâmetros de rotas
- * http://localhost:3333/settings/1
- * 
- * Query Params -> Filtros e busca
- * http://localhost:3333/settings/1?search=algumacoisa
- * 
- * Body Params -> Passa objetos dentro da requisição = JSON {
- * 
- * }
- */
+const settingController = new SettingController();
 
-routes.post("/settings", async (request, response) => {
-    const { chat, username } = request.body;
-    const settingsRepository = getCustomRepository(SettingRepository);
-
-    const settings = settingsRepository.create({
-        chat,
-        username
-    })
-
-    await settingsRepository.save(settings);
-
-    return response.json(settings);
-});
+routes.post("/settings", settingController.create);
 
 export { routes };
